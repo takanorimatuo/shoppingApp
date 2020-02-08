5501014415a3a206a1c8873ba3530cf909ab3dcd
ryoya1993 <ryoya19930270@yahoo.co.jp>
2020-02-08 21:09:35 +0900

    Update README.md

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,8 @@
+<!doctype html>
+<html lang="ja-JP" class>
+	<head> == $0
+		<! -- Anti-flicker snippet  -->
+		<style>.async-hide　{ opacity: 0 !important}</style>
+		<script type="text/javascript" async src="http"
+	</head>
+</html>
