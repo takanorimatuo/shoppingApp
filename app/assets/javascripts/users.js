87f0afbdd17bc1a797960fadca7f912dc003c078
ryoya1993 <ryoya19930270@yahoo.co.jp>
2020-02-09 19:25:08 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,10 +1,26 @@
 <html lang="ja-JP" class>
-	<head> == $0
-		<style>.async-hide { opacity: 0 !important}</style>
+	<head>
+		<style>.async-hide { opacity: 0 !important }</style>
 		<script type="text/javascript" async src="http://www.google-analytics.com/analytics.js">
 		</script>
 		<script type="text/javascript" async="" src="https://www.google-analytics.com/plugins/ua/ec.js"></script>
 		<script type="text/javascript" async="" src="https://www.google-analytics.com/gtm/js?id=GTM-WVG9WTC&amp;cid=937295055.1580631265"></script>
 		<script src="https://connect.facebook.net/signals/config/1964320460558899?v=2.9.15&amp;r=stable" async=""></script>
-	</head>
+		<script async="" src="https://connect.facebook.net/en_US/fbevents.js"></script>
+		<script type="text/javascript" async="" src="https://www.google-analytics.com/analytics.js"></script>
+		<script async="" src="https://www.googletagmanager.com/gtm.js?id=GTM-KLPT5L5"></script>
+		<script type="text/javascript" async="" src="https://www.gstatic.com/recaptcha/releases/vJuUWXolyYJx1oqUVmpPuryQ/recaptcha__ja.js"></script>
+		<script>
+			(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+      h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+      (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+      })(window,document.documentElement,'async-hide','dataLayer',4000,
+      {'GTM-KLPT5L5':true});
+		</script>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<title>会員情報入力 - メルカリ スマホでかんたん フリマアプリ</title>
+		<meta name="description" content="CMで話題！フリマアプリ「メルカリ」は、スマホから誰でも簡単に売り買いが楽しめるフリマアプリです。購入時はクレジットカード・キャリア決済・コンビニ・銀行ATMで支払いでき、品物が届いてから出品者に入金される独自システムで安心です。"></meta>
+		</meta>
+	  </head>
 </html>
