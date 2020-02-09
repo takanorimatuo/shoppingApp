33bbe47e115f0fbad5ea007758a8c1381b8b8dd6
ryoya1993 <ryoya19930270@yahoo.co.jp>
2020-02-09 15:42:43 +0900

    Update users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,8 +1,10 @@
-<!doctype html>
 <html lang="ja-JP" class>
 	<head> == $0
-		<! -- Anti-flicker snippet  -->
-		<style>.async-hide　{ opacity: 0 !important}</style>
-		<script type="text/javascript" async src="http://"
+		<style>.async-hide { opacity: 0 !important}</style>
+		<script type="text/javascript" async src="http://www.google-analytics.com/analytics.js">
+		</script>
+		<script type="text/javascript" async="" src="https://www.google-analytics.com/plugins/ua/ec.js"></script>
+		<script type="text/javascript" async="" src="https://www.google-analytics.com/gtm/js?id=GTM-WVG9WTC&amp;cid=937295055.1580631265"></script>
+		<script src="https://connect.facebook.net/signals/config/1964320460558899?v=2.9.15&amp;r=stable" async=""></script>
 	</head>
 </html>
