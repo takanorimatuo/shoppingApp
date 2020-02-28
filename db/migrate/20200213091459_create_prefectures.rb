de6c49a1c3992517c51049c678393ff642aa7cd5
takanorimatuo <regional.portals.wakayama@gmail.com>
2020-02-28 20:54:02 +0900

    active_hashの導入

--- db/migrate/20200213091459_create_prefectures.rb
+++ db/migrate/20200213091459_create_prefectures.rb
@@ -2,7 +2,7 @@ class CreatePrefectures < ActiveRecord::Migration[5.2]
   def change
     create_table :prefectures(active_hash) do |t|
       t.string   :prefecture_name, null: false
-      t.string   :user_id, null: false foreign_key: true
+      t.string   :user_id, null: false, foreign_key: true
       t.timestamps
     end
   end
