0650d03592922c51d5ce8df708afec2f483064c1
takanorimatuo <regional.portals.wakayama@gmail.com>
2020-02-29 19:57:53 +0900

    usersテーブルの修正

--- db/migrate/20200224111421_devise_create_users.rb
+++ db/migrate/20200224111421_devise_create_users.rb
@@ -5,7 +5,7 @@ class CreateShoppingapps < ActiveRecord::Migration[5.2]
       t.string      :last_name, null: false
       t.string      :nickname, null: false
       t.string      :first_kana, null: false
-      t.string      :last_name, null: false
+      t.string      :last_kana, null: false
       t.string      :password, null: false
       t.date        :birth_day, null: false
       t.string      :email, null: false
