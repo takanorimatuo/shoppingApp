d942125966b9815a5278555726faea7b0645e704
bz0108rock-on <57309243+bz0108rock-on@users.noreply.github.com>
2020-02-29 19:38:10 +0900

    不要migrationファイルの削除

--- db/migrate/20200213091459_create_prefectures.rb
+++
@@ -1,9 +0,0 @@
-class CreatePrefectures < ActiveRecord::Migration[5.2]
-  def change
-    create_table :prefectures do |t|
-      t.string   :prefecture_id, null: false, foreign_key: true
-      t.string   :user_id, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
