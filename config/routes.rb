982ae703868987290fd86e37d9be57fe92385e51
bz0108rock-on <57309243+bz0108rock-on@users.noreply.github.com>
2020-03-02 23:21:55 +0900

    ユーザーマイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'sells#index'
+  get 'mypage' => 'mypage#index'
 end
--- db/migrate/20200224111421_devise_create_users.rb
+++ db/migrate/20200224111421_devise_create_users.rb
@@ -1,4 +1,4 @@
-class CreateShoppingapps < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string      :first_name, null: false
