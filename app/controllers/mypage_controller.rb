8fbed2860194886d71b81db8622874a8ee8e5563
takanorimatuo <regional.portals.wakayama@gmail.com>
2020-03-15 20:35:03 +0900

    ログイン・新規登録サーバーサイド実装

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,6 @@
 class MypageController < ApplicationController
   def index
+    user = User.find(params[:id])
+    @nicknmae = user.nickname
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :mypage, only: [:index]
   resources :users, only: [:new]
+  resources :addresses, except: [:index, :show]
 end
