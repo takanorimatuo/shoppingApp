c41fa23cb0a302833a4fd2e324bdcba4eee8f0cc
takanorimatuo <regional.portals.wakayama@gmail.com>
2020-03-27 20:38:59 +0900

    includesの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   def index
     @products = Product.order("created_at DESC").limit(3)
     @category = Product.where(category_id: "1").limit(3)
-    # @products = Product.includes(:user).order("created_at DESC").limit(3)
-    # @category = Product.includes(:user).where(category_id: "1").limit(3)
   end
   
   def new
