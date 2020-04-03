235e4d7170e4b728b8dcbc51fe797c8ac37c058f
bz0108rock-on <57309243+bz0108rock-on@users.noreply.github.com>
2020-04-03 22:01:56 +0900

    edit action
    投稿者だけが編集ページに遷移できるように。
    ログアウトボタンのcss修正。

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,17 +14,28 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
     @product.valid?
     if @product.images.present? && @product.save
+      @product.update(seller_id: current_user.id)
       redirect_to root_path
     else
       @product.images.build
-      render "new"
+      render :new
     end
   end
 
   def edit
+    @product = Product.find(params[:id])
+    redirect_to root_path if @product.seller_id != current_user.id
+    @images = @product.images.build
   end
 
   def update
+    @product = Product.find(params[:id])
+    if product.seller_id == current_user.id
+      @product.update(product_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -32,8 +43,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-
-    params.require(:product).permit(:product_name, :text, :category_id, :brand, :status_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, images_attributes: [:src, :id]).merge(seller_id: current_user.id)
-
+    params.require(:product).permit(:product_name, :text, :category_id, :brand, :status_id, :delivery_charge_id, :prefecture_id, :delivery_day_id, :price, images_attributes: [:src, :_destroy, :id]).merge(seller_id: current_user.id)
   end
 end
